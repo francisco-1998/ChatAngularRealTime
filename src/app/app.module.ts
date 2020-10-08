2ed9bbb4bd624c38efce035fa321fe8b282e501c
francisco-1998 <francmarinc@gmail.com>
2020-10-08 11:02:00 -0500

    configuracion del proyecto - seccion 5 : Login y mejoras al chat

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,9 @@ import { FooterComponent } from './components/footer/footer.component';
 import { ChatComponent } from './components/chat/chat.component';
 import { environment } from '../environments/environment';
 import { FormsModule } from '@angular/forms';
+import { ListaUsuariosComponent } from './components/lista-usuarios/lista-usuarios.component';
+import { LoginComponent } from './pages/login/login.component';
+import { MensajesComponent } from './pages/mensajes/mensajes.component';
 
 
 const config: SocketIoConfig = { url: environment.socketURL, options: {} };
@@ -17,7 +20,10 @@ const config: SocketIoConfig = { url: environment.socketURL, options: {} };
   declarations: [
     AppComponent,
     FooterComponent,
-    ChatComponent
+    ChatComponent,
+    ListaUsuariosComponent,
+    LoginComponent,
+    MensajesComponent
   ],
   imports: [
     BrowserModule,
